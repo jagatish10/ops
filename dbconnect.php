7fd253eaa222cae2faf77388e4e071e1546333bf
Jagatish <jagatishtorres@yahoo.com>
2021-01-11 02:57:34 +0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-$host="remotemysql.com";
-$username="WUAvaC7rMr";
-$pass="n1L4r7BCMk";
+$host="sql12.freesqldatabase.com";
+$username="sql12386185";
+$pass="dWtsJEfg3j";
 $dbconn=mysqli_connect($host,$username,$pass) or die ("CONNECTION FAIL!!!!!");
-$db=mysqli_select_db($dbconn,"WUAvaC7rMr");
+$db=mysqli_select_db($dbconn,"sql12386185");
 
 ?>
