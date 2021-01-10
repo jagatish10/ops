d60bd1f041bafabe4b4a47d64d67e032426d3ecf
Jagatish <jagatishtorres@yahoo.com>
2021-01-11 02:03:17 +0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-$host="localhost";
-$username="root";
-$pass="";
+$host="remotemysql.com";
+$username="WUAvaC7rMr";
+$pass="n1L4r7BCMk";
 $dbconn=mysqli_connect($host,$username,$pass) or die ("CONNECTION FAIL!!!!!");
-$db=mysqli_select_db($dbconn,"ops");
+$db=mysqli_select_db($dbconn,"WUAvaC7rMr");
 
 ?>
