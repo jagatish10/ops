41fb049f073269500d8618425758757e891c3ce3
Jagatish <jagatishtorres@yahoo.com>
2021-01-12 04:46:53 +0800

    Update addadmin.php

--- admin/addadmin.php
+++ admin/addadmin.php
@@ -66,7 +66,7 @@ $currentuser=$_SESSION['ADMIN_ID'];
                 <div class="col-md-12">
                     <div class="navbar-collapse collapse ">
 						<ul id="menu-top" class="nav navbar-nav navbar-right">
-                            <li><a href="utama1.php" class="menu-top-active">MENU</a></li>
+                            <li><a href="../admin/utama1.php">ACCESSIBILITY</a></li>
                         </ul>
 						<ul id="menu-top" class="nav navbar-nav navbar-left" style="padding-left:150px">
 
