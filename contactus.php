4de010d8cf8bdfbc4344da6e20f02be9b5ea6ad0
Jagatish <jagatishtorres@yahoo.com>
2021-01-12 04:47:31 +0800

    Create contactus.php

---
+++ contactus.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>HOME</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="user/assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="user/assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="user/assets/css/style.css" rel="stylesheet" />
+    <link href="contactus.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+    <link rel="icon" type="image/png" href="user/assets/images/icons/favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="user/assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="user/assets/vendor/animate/animate.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="user/assets/vendor/css-hamburgers/hamburgers.min.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="user/assets/vendor/select2/select2.min.css">
+
+    <!-- <link rel="stylesheet" type="text/css" href="assets/css/util.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css"> -->
+
+    <link rel="stylesheet" type="text/css" href="user/assets/css/util1.css">
+    <link rel="stylesheet" type="text/css" href="user/assets/css/main1.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
+</head>
+<body>
+    <div class="navbar navbar-inverse set-radius-zero" >
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand">
+                  <div class="login100-pic js-tilt" data-tilt>
+                    <img src="user/assets/img/opslogo.png" alt="img" />
+                  </div>
+                </a>
+
+            </div>
+        </div>
+    </div>
+    <!-- LOGO HEADER END-->
+    <section class="menu-section">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                         <li><a href="user/login.php">USER</a></li>
+                         <li><a href="admin/login.php">ADMIN</a></li>
+                       </ul>
+                         <ul id="menu-top" class="nav navbar-nav navbar-centre" style="padding-left:150px">
+                           <li><a href="index.php">HOME</a></li>
+                           <li><a href="price.php">PRICING</a></li>
+                           <li><a href="contactus.php">CONTACT US</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+     <!-- MENU SECTION END-->
+    <div class="content-wrapper">
+         <div class="container">
+        <div class="row pad-botm">
+            <div class="col-md-12">
+                <h4 class="header">REACH US AT</h4>
+                            </div>
+
+        </div>
+
+        <div class="contact-wrapper clearfix">
+
+        <figure class="snip1574" class="login100-pic js-tilt" data-tilt>
+          <img src="penang.jpg" alt="profile-sample2" />
+          <figcaption>
+            <h3>Penang</h3>
+          </figcaption>
+        <h3>ops_penang@gmail.com</h3>
+        <h3>012-3456789</h3>
+        </figure>
+        <figure class="snip1574" class="login100-pic js-tilt" data-tilt><img src="melaka.jpg" alt="profile-sample7" />
+          <figcaption>
+            <h3>Melaka</h3>
+          </figcaption>
+          <h3>ops_melaka@gmail.com</h3>
+          <h3>012-3456789</h3>
+        </figure>
+        <figure class="snip1574" class="login100-pic js-tilt" data-tilt><img src="kedah.jpg" alt="profile-sample6" />
+          <figcaption>
+            <h3>Kedah</h3>
+          </figcaption>
+          <h3>ops_kedah@gmail.com</h3>
+          <h3>012-3456789</h3>
+        </figure>
+      </figure>
+      <figure class="snip1574" class="login100-pic js-tilt" data-tilt><img src="perak.jpg" alt="profile-sample6" />
+        <figcaption>
+          <h3>Perak</h3>
+        </figcaption>
+        <h3>ops_perak@gmail.com</h3>
+        <h3>012-3456789</h3>
+      </figure>
+      <figure class="snip1574" class="login100-pic js-tilt" data-tilt><img src="kl.jpg" alt="profile-sample6" />
+        <figcaption>
+          <h3>Kuala Lumpur</h3>
+        </figcaption>
+        <h3>ops_kl@gmail.com</h3>
+        <h3>012-3456789</h3>
+      </figure>
+      <figure class="snip1574" class="login100-pic js-tilt" data-tilt><img src="jb.jpg" alt="profile-sample6" />
+        <figcaption>
+          <h3>Johor</h3>
+        </figcaption>
+        <h3>ops_johor@gmail.com</h3>
+        <h3>012-3456789</h3>
+      </figure>
+
+</div>
+
+             </div>
+
+    </div>
+    </div>
+     <!-- CONTENT-WRAPPER SECTION END-->
+    <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Designed by : OPS
+                </div>
+
+            </div>
+        </div>
+    </section>
+      <!-- FOOTER SECTION END-->
+    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY  -->
+    <script src="user/assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="user/assets/js/bootstrap.js"></script>
+      <!-- CUSTOM SCRIPTS  -->
+    <script src="user/assets/js/custom.js"></script>
+
+    <script src="user/assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+  <!--===============================================================================================-->
+    <script src="user/assets/vendor/bootstrap/js/popper.js"></script>
+    <script src="user/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+  <!--===============================================================================================-->
+    <script src="user/assets/vendor/select2/select2.min.js"></script>
+  <!--===============================================================================================-->
+    <script src="user/assets/vendor/tilt/tilt.jquery.min.js"></script>
+
+    <script >
+      $('.js-tilt').tilt({
+        scale: 1.1
+      })
+    </script>
+
+  <script src=user/assets/"js/main.js"></script>
+
+
+</body>
+</html>
