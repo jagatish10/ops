fb1839ddd0cd0d5c85cba41aaf8fd38bd7512911
Jagatish <jagatishtorres@yahoo.com>
2021-01-12 05:18:50 +0800

    Update printing.php

--- admin/printing.php
+++ admin/printing.php
@@ -29,7 +29,7 @@ $kuirirun=mysqli_query($dbconn,$kuiri) or die ("Can Not Continue");
     <!--[if IE]>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <![endif]-->
-    <title>PRINTING</title>
+    <title>PAYMENT STATUS</title>
     <!-- BOOTSTRAP CORE STYLE  -->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <!-- FONT AWESOME STYLE  -->
@@ -38,6 +38,23 @@ $kuirirun=mysqli_query($dbconn,$kuiri) or die ("Can Not Continue");
     <link href="assets/css/style.css" rel="stylesheet" />
     <!-- GOOGLE FONT -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+    <link rel="icon" type="image/png" href="assets/images/icons/favicon.ico"/>
+  <!--===============================================================================================-->
+  <!--  <link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap/css/bootstrap.min.css"> -->
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="assets/vendor/animate/animate.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="assets/vendor/css-hamburgers/hamburgers.min.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="assets/vendor/select2/select2.min.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="assets/css/util1.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/main1.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link href="assets/css/form.css" rel="stylesheet" media="all"
 
 </head>
 <body>
@@ -50,8 +67,9 @@ $kuirirun=mysqli_query($dbconn,$kuiri) or die ("Can Not Continue");
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand">
-
-                    <img src="assets/img/tdt.png" />
+                  <div class="login100-pic js-tilt" data-tilt>
+                  <img src="assets/img/opslogo.png"/>
+                  </div>
                 </a>
             </div>
 			<div class="right-div">
@@ -66,7 +84,7 @@ $kuirirun=mysqli_query($dbconn,$kuiri) or die ("Can Not Continue");
                 <div class="col-md-12">
                     <div class="navbar-collapse collapse ">
 						<ul id="menu-top" class="nav navbar-nav navbar-right">
-                            <li><a href="utama.php" class="menu-top-active">MENU</a></li>
+                            <li><a href="utama1.php" class="menu-top-active">MENU</a></li>
                         </ul>
                         <ul id="menu-top" class="nav navbar-nav navbar-right">
                           <li><a href="../admin/laporan.php" >Overall</a></li>
@@ -75,7 +93,7 @@ $kuirirun=mysqli_query($dbconn,$kuiri) or die ("Can Not Continue");
                           <li><a href="../admin/printingorder.php">Printing</a></li>
                           <li><a href="../admin/finished.php">Finished</a></li>
                        </ul>
-						<ul id="menu-top" class="nav navbar-nav navbar-left">
+						<ul id="menu-top" class="nav navbar-nav navbar-left" style="padding-left:150px">
                             <li><a>Hi,<i><?php echo $_SESSION['ADMIN_USERNAME'];?></a></i></li>
                         </ul>
                     </div>
@@ -85,114 +103,205 @@ $kuirirun=mysqli_query($dbconn,$kuiri) or die ("Can Not Continue");
         </div>
     </section>
      <!-- MENU SECTION END-->
-    <div class="content-wrapper">
-         <div class="container">
-        <div class="row pad-botm">
-            <div class="col-md-12">
-                <h4 class="header-line"></h4>
+     <div class="content-wrapper">
+          <div class="container">
+         <div class="row pad-botm">
+             <div class="col-md-12">
 
                             </div>
 
         </div>
-				  <div class="col-md-7 col-sm-7 col-xs-7">
-                      <div class="panel panel-success">
 
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <form method="post" action="prosestatus1.php">
-                                <table class="table table-striped table-bordered table-hover">
-        <script type="text/javascript">
+        <div class="wrapper wrapper--w790">
+            <div class="card card-5">
+                <div class="card-heading">
+                    <h2 class="title">PAYMENT STATUS</h2>
+                </div>
+                <div class="card-body">
+                    <form method="post" action="prosestatus1.php">
 
-                                    function calculate(f) {
+                      <script type="text/javascript">
 
-                                      var documentprice=f.price.value;
-                                      var deliverycharge=f.dprice.value;
-                                      var totalprice= (+documentprice) + (+deliverycharge);
-                                      //var charge=f.charge.options[f.charge.selectedIndex].value;
-                                      //var total=totalp*copies*price;
-                                      f.total.value=totalprice+".00";
-                                      //f.due.value=myr(0.00*total);
-                                    }
-         </script>
-									<tbody>
-                    <tr>
-										    <td style="font-weight:bold" align="right">USERNAME :</td>
-											<td><input type="text" readonly name="Matricnumber" required="required" value="<?php echo $ID;?>"></td>
-									    </tr>
-                      <tr>
-										    <td style="font-weight:bold" align="right">FILENAME :</td>
-											<td><input type="text" readonly name="Download" required="required" value="<?php echo $Download;?>"></td>
-									    </tr>
-									    <tr>
-										    <td style="font-weight:bold" align="right">COPIES :</td>
-											<td><input type="text" readonly name="amount" required="required" value="<?php echo $Copies;?>"></td>
-									    </tr>
-										<tr>
-										    <td style="font-weight:bold" align="right">TOTAL PAGE :</td>
-											<td><input type="text" readonly name="totalp" required="required" value="<?php echo $totalp;?>"></td>
-									    </tr>
-										<tr>
-										    <td style="font-weight:bold" align="right">COLOUR :</td>
-											<td><input type="text" readonly name="colour" required="required" value="<?php echo $Colour;?>"></td>
-									    </tr>
-										<tr>
-										    <td style="font-weight:bold" align="right">PAGE ORDER :</td>
-											<td><input type="text" readonly name="order" required="required" value="<?php echo $Order;?>"></td>
-									    </tr>
-										<tr>
-										    <td style="font-weight:bold" align="right">PAGE LAYOUT :</td>
-											<td><input type="text" readonly name="layout" required="required" value="<?php echo $Layout;?>"></td>
-									    </tr>
-										<tr>
-										    <td style="font-weight:bold" align="right">PAGE PER SHEET :</td>
-											<td><input type="text" readonly name="sheet" required="required" value="<?php echo $PerSheet;?>"></td>
-									    </tr>
-                      <tr>
-                          <td style="font-weight:bold" align="right">ADDITIONAL COMMENTS :</td>
-                        <td><textarea readonly name="comment" rows="5" cols="40" value="<?php echo $Comment;?>"></textarea></td>
-                        </tr>
-                        <tr>
-                          <td style="font-weight:bold" align="right">COLLECTION :</td>
-                        <td><input type="text" readonly name="collection" required="required" value="<?php echo $Collection;?>"></td>
-                        </tr>
-                      <tr>
-                        <td style="font-weight:bold" align="right">DOCUMENT PRICE :</td>
-                      <td><input type="text" name="price" required="required" /></td>
-                      </tr>
-                    <tr>
-                        <td style="font-weight:bold" align="right">DELIVERY CHARGE :</td>
-                      <td><input type="text" name="dprice" required="required" /></td>
-                      </tr>
-                      <tr>
-                          <td style="font-weight:bold" align="right">TOTAL PRICE RM:</td>
-                        <td><input type=text name=total size=10 value="-">
-                        <input type=button value=Calculate onClick="calculate(this.form);"></td>
-                        </tr>
-                        <tr>
-    										    <td style="font-weight:bold" align="right">CURRENT STATUS :</td>
-<?php
-while($row=mysqli_fetch_array($kuirirun)){
-?>
-    											<td><input type="text" readonly value="<?php echo $row[0];?>"></td>
-<?php } ?>
-    									    </tr>
-                    <tr>
-										    <td style="font-weight:bold" align="right">UPDATING STATUS :</td>
-											<td><input type="text" readonly name="sts" value="Waiting for Payment"/></td>
-									    </tr>
-<input type="hidden" name="file_link" value="<?php echo $Download;?>">
-										<tr>
-											<td colspan=2 align="center"><input type="submit" name="submit" required="required" value="UPDATE"/>
-														<a href="<?php echo $Download;?>" target="_blank"><img width="40"src="../images/printlogo.jpg"></a></td>
-										</tr>
-									</tbody>
-                                </table>
-								</form>
-                            </div>
+                                                  function calculate(f) {
+
+                                                    var documentprice=f.price.value;
+                                                    var deliverycharge=f.dprice.value;
+                                                    var totalprice= (+documentprice) + (+deliverycharge);
+                                                    //var charge=f.charge.options[f.charge.selectedIndex].value;
+                                                    //var total=totalp*copies*price;
+                                                    f.total.value=totalprice+".00";
+                                                    //f.due.value=myr(0.00*total);
+                                                  }
+                       </script>
+
+
+                      <div class="form-row">
+                        <div class="name">USERNAME</div>
+                        <div class="value">
+                        <div class="wrap-input100 validate-input">
+                          <input class="input100" type="text" readonly name="Matricnumber" required="required" value="<?php echo $ID;?>">
+                          <span class="focus-input100"></span>
                         </div>
+                      </div>
                     </div>
-             </div>
-             </div>
+
+                    <div class="form-row">
+                      <div class="name">FILENAME</div>
+                      <div class="value">
+                      <div class="wrap-input100 validate-input">
+                        <input class="input100" type="text" readonly name="Download" required="required" value="<?php echo $Download;?>">
+                        <span class="focus-input100"></span>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="form-row">
+                    <div class="name">COPIES</div>
+                    <div class="value">
+                    <div class="wrap-input100 validate-input">
+                      <input class="input100" type="text" readonly name="amount" required="required" value="<?php echo $Copies;?>">
+                      <span class="focus-input100"></span>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="form-row">
+                  <div class="name">TOTAL PAGE</div>
+                  <div class="value">
+                  <div class="wrap-input100 validate-input">
+                    <input class="input100" type="text" readonly name="totalp" required="required" value="<?php echo $totalp;?>">
+                    <span class="focus-input100"></span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="name">COLOUR</div>
+                <div class="value">
+                <div class="wrap-input100 validate-input">
+                  <input class="input100" type="text" readonly name="colour" required="required" value="<?php echo $Colour;?>">
+                  <span class="focus-input100"></span>
+                </div>
+              </div>
+            </div>
+
+            <div class="form-row">
+              <div class="name">PAGE<br>ORDER</div>
+              <div class="value">
+              <div class="wrap-input100 validate-input">
+                <input class="input100" type="text" readonly name="order" required="required" value="<?php echo $Order;?>">
+                <span class="focus-input100"></span>
+              </div>
+            </div>
+          </div>
+
+          <div class="form-row">
+            <div class="name">PAGE<br>LAYOUT</div>
+            <div class="value">
+            <div class="wrap-input100 validate-input">
+              <input class="input100" type="text" readonly name="layout" required="required" value="<?php echo $Layout;?>">
+              <span class="focus-input100"></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="form-row">
+          <div class="name">PAGE<br>PER<br>SHEET</div>
+          <div class="value">
+          <div class="wrap-input100 validate-input">
+            <input class="input100" type="text" readonly name="sheet" required="required" value="<?php echo $PerSheet;?>">
+            <span class="focus-input100"></span>
+          </div>
+        </div>
+      </div>
+
+      <div class="form-row">
+          <div class="name">ADDITIONAL COMMENTS</div>
+          <div class="value">
+            <div class="wrap-input100 ">
+              <input class="input100" readonly name="comment" value="<?php echo $Comment;?>"></input>
+              <span class="focus-input100"></span>
+            </div>
+          </div>
+      </div>
+
+      <div class="form-row">
+          <div class="name">COLLECTION</div>
+          <div class="value">
+            <div class="wrap-input100 ">
+              <input class="input100" type="text" readonly name="collection" required="required" value="<?php echo $Collection;?>"></input>
+              <span class="focus-input100"></span>
+            </div>
+          </div>
+      </div>
+
+      <div class="form-row">
+          <div class="name">DOCUMENT PRICE</div>
+          <div class="value">
+            <div class="wrap-input100 ">
+              <input class="input100" type="text" name="price" required="required"/></input>
+              <span class="focus-input100"></span>
+            </div>
+          </div>
+      </div>
+
+      <div class="form-row">
+          <div class="name">DELIVERY CHARGE</div>
+          <div class="value">
+            <div class="wrap-input100 ">
+              <input class="input100" type="text" name="dprice" required="required" /></input>
+              <span class="focus-input100"></span>
+            </div>
+          </div>
+      </div>
+
+
+      <div class="form-row">
+          <div class="name">TOTAL PRICE RM:</div>
+          <div class="value">
+            <div class="input-group">
+              <input class="input100" type=text name=total size=10 value="-">
+              <input class="btn btn--radius-2 btn--red" type=button  value=Calculate onClick="calculate(this.form);">
+            </div>
+          </div>
+      </div>
+
+      <div class="form-row">
+          <div class="name">CURRENT STATUS</div>
+          <div class="value">
+            <div class="wrap-input100 ">
+              <?php
+              while($row=mysqli_fetch_array($kuirirun)){
+              ?>
+              <input class="input100" <input type="text" readonly value="<?php echo $row[0];?>"></input>
+              <?php } ?>
+              <span class="focus-input100"></span>
+            </div>
+          </div>
+      </div>
+
+
+      <div class="form-row">
+          <div class="name">UPDATING STATUS</div>
+          <div class="value">
+            <div class="wrap-input100 ">
+              <input class="input100" type="text" readonly name="sts" value="Waiting for Payment"/></input>
+              <span class="focus-input100"></span>
+            </div>
+          </div>
+      </div>
+
+      <input type="hidden" name="file_link" value="<?php echo $Download;?>">
+      										<tr>
+      											<td colspan=2 align="center"><input class="btn btn--radius-2 btn--red" type="submit" name="submit" required="required" value="UPDATE"/></td>
+      														<a href="<?php echo $Download;?>" target="_blank"><img width="40"src="../images/printlogo.jpg">DOWLOAD FILE</a>
+      										</tr>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+
 
     </div>
     </div>
@@ -201,7 +310,7 @@ while($row=mysqli_fetch_array($kuirirun)){
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                   &copy; Designed by : TDT
+                   &copy; Designed by : OPS
                 </div>
 
             </div>
@@ -215,6 +324,23 @@ while($row=mysqli_fetch_array($kuirirun)){
     <script src="assets/js/bootstrap.js"></script>
       <!-- CUSTOM SCRIPTS  -->
     <script src="assets/js/custom.js"></script>
+
+    <script src="assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+  <!--===============================================================================================-->
+    <script src="assets/vendor/bootstrap/js/popper.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+  <!--===============================================================================================-->
+    <script src="assets/vendor/select2/select2.min.js"></script>
+  <!--===============================================================================================-->
+    <script src="assets/vendor/tilt/tilt.jquery.min.js"></script>
+
+    <script >
+      $('.js-tilt').tilt({
+        scale: 1.1
+      })
+    </script>
+
+  <script src=assets/"js/main.js"></script>
 
 </body>
 </html>
