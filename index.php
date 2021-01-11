bbf8bb16e783c860990b2298b4aaf13f0ee2af56
Jagatish <jagatishtorres@yahoo.com>
2021-01-12 04:47:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,8 +15,27 @@
     <link href="user/assets/css/font-awesome.css" rel="stylesheet" />
     <!-- CUSTOM STYLE  -->
     <link href="user/assets/css/style.css" rel="stylesheet" />
+    <link href="slider.css" rel="stylesheet" />
     <!-- GOOGLE FONT -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+    <link rel="icon" type="image/png" href="user/assets/images/icons/favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="user/assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="user/assets/vendor/animate/animate.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="user/assets/vendor/css-hamburgers/hamburgers.min.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="user/assets/vendor/select2/select2.min.css">
+
+    <!-- <link rel="stylesheet" type="text/css" href="assets/css/util.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css"> -->
+
+    <link rel="stylesheet" type="text/css" href="user/assets/css/util1.css">
+    <link rel="stylesheet" type="text/css" href="user/assets/css/main1.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+
 
 </head>
 <body>
@@ -29,8 +48,9 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand">
-
-                    <img src="user/assets/img/opslogo.png" />
+                  <div class="login100-pic js-tilt" data-tilt>
+                    <img src="user/assets/img/opslogo.png" alt="img" />
+                  </div>
                 </a>
 
             </div>
@@ -46,11 +66,10 @@
                          <li><a href="user/login.php">USER</a></li>
                          <li><a href="admin/login.php">ADMIN</a></li>
                        </ul>
-                         <ul id="menu-top" class="nav navbar-nav navbar-right">
-                            <li><a href="user/login.php" >HOME</a></li>
-                            <li><a href="admin/login.php">USER MANUAL</a></li>
-                            <li><a href="admin/login.php">ABOUT</a></li>
-                            <li><a href="admin/login.php">CONTACT US</a></li>
+                         <ul id="menu-top" class="nav navbar-nav navbar-centre" style="padding-left:150px">
+                            <li><a href="index.php">HOME</a></li>
+                            <li><a href="price.php">PRICING</a></li>
+                            <li><a href="contactus.php">CONTACT US</a></li>
                         </ul>
                     </div>
                 </div>
@@ -63,13 +82,57 @@
          <div class="container">
         <div class="row pad-botm">
             <div class="col-md-12">
-                <h4 class="header">Online Printing System</h4>
+                <!-- <h4 class="header">Online Printing System</h4> -->
+                <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+                <br><div class="elfsight-app-12a12f6c-8ab9-4948-a537-f4e3a0156c46"></div>
 
-                            </div>
 
-        </div>
+  </div>
+  <div class="container main-container">
+
+  	<div id="carousel-example-generic" class="carousel slide" data-interval="7000">
+  		<!-- Indicators -->
+  		<ol class="carousel-indicators">
+  			<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+  			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+  			<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  		</ol>
+
+  		<!-- Wrapper for slides -->
+  		<div class="carousel-inner" role="listbox">
+
+  			<!-- First slide -->
+  			<div class="item active deepskyblue">
+  			</div> <!-- /.item -->
+
+  			<!-- Second slide -->
+  			<div class="item skyblue">
+  			</div><!-- /.item -->
+
+  			<!-- Third slide -->
+  			<div class="item darkerskyblue">
+  			</div><!-- /.item -->
+
+  		</div><!-- /.carousel-inner -->
+
+  		<!-- Controls -->
+  		<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+  			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+  			<span class="sr-only">Previous</span>
+  		</a>
+  		<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+  			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+  			<span class="sr-only">Next</span>
+  		</a>
+  	</div><!-- /.carousel -->
+
+  </div><!-- /.container -->
+
+        <br>
         <script src="https://apps.elfsight.com/p/platform.js" defer></script>
         <div class="elfsight-app-ba6162b0-283d-43e9-8715-8b2c50e9cba2"></div>
+
+
 
              </div>
 
@@ -94,6 +157,26 @@
     <script src="user/assets/js/bootstrap.js"></script>
       <!-- CUSTOM SCRIPTS  -->
     <script src="user/assets/js/custom.js"></script>
+
+    <script src="user/assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+  <!--===============================================================================================-->
+    <script src="user/assets/vendor/bootstrap/js/popper.js"></script>
+    <script src="user/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+  <!--===============================================================================================-->
+    <script src="user/assets/vendor/select2/select2.min.js"></script>
+  <!--===============================================================================================-->
+    <script src="user/assets/vendor/tilt/tilt.jquery.min.js"></script>
+
+
+    <script >
+      $('.js-tilt').tilt({
+        scale: 1.1
+      })
+    </script>
+
+
+  <script src=user/assets/"js/main.js"></script>
+
 
 </body>
 </html>
