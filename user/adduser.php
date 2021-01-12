7b9ea9a9b9baa8c861be93084558eac23f20f594
Jagatish <jagatishtorres@yahoo.com>
2021-01-13 04:03:23 +0800

    Update adduser.php

--- user/adduser.php
+++ user/adduser.php
@@ -63,7 +63,7 @@
                 <div class="col-md-12">
                     <div class="navbar-collapse collapse ">
 						<ul id="menu-top" class="nav navbar-nav navbar-right">
-                            <li><a href="home.php" class="menu-top-active">MENU</a></li>
+                            <li><a href="../index.php" class="menu-top-active">HOME</a></li>
                         </ul>
                     </div>
                 </div>
