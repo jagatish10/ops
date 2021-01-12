b5109b74b186dea6af4a8e5bd9b409611971e04c
Jagatish <jagatishtorres@yahoo.com>
2021-01-13 04:17:12 +0800

    Update prosesedituser.php

--- user/prosesedituser.php
+++ user/prosesedituser.php
@@ -49,7 +49,7 @@ $currentuser=$_SESSION['USER_NAME'];
                 <div class="col-md-12">
                     <div class="navbar-collapse collapse ">
 						<ul id="menu-top" class="nav navbar-nav navbar-right">
-                            <li><a href="home.php" class="menu-top-active">MENU</a></li>
+                            <li><a href="utama.php" class="menu-top-active">HOME</a></li>
                         </ul>
                     </div>
                 </div>
