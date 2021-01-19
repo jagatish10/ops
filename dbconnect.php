0c35e50988fc5091f709e6e6b98245094456fbb2
Jagatish <jagatishtorres@yahoo.com>
2021-01-19 19:19:13 +0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-$host="sql12.freesqldatabase.com";
-$username="sql12386185";
-$pass="dWtsJEfg3j";
+$host="freedb.tech";
+$username="freedbtech_ops";
+$pass="ops12345";
 $dbconn=mysqli_connect($host,$username,$pass) or die ("CONNECTION FAIL!!!!!");
-$db=mysqli_select_db($dbconn,"sql12386185");
+$db=mysqli_select_db($dbconn,"freedbtech_ops");
 
 ?>
