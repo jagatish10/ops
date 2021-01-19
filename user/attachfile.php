9efd329217c5a1aeceb677a664f1a1385ce9b7b0
Jagatish <jagatishtorres@yahoo.com>
2021-01-19 21:14:36 +0800

    Update attachfile.php

--- user/attachfile.php
+++ user/attachfile.php
@@ -72,6 +72,7 @@ $kuirirun=mysqli_query($dbconn,$kuiri) or die ("Can Not Continue");
                 <div class="col-md-12">
                     <div class="navbar-collapse collapse ">
                         <ul id="menu-top" class="nav navbar-nav navbar-right">
+                          <li><a href="../user/utama.php" >USER HOME</a></li>
                             <li><a href="utama1.php" >FILE MANAGEMENT</a></li>
                         </ul>
 						<ul id="menu-top" class="nav navbar-nav navbar-left" style="padding-left:150px">
